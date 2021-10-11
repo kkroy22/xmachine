948c27a8637fad006dae3dd22666b6413895d211
Kiran Kumar Roy <3953740+kkroy22@users.noreply.github.com>
2021-10-11 09:32:26 +0530

    Update external.sh

--- settings/bspwm/external.sh
+++ settings/bspwm/external.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate right --output VGA-0 --mode 1920x1080 --pos 1080x840 --rotate normal
+xrandr --output VGA-0 --mode 1024x768 --pos 3000x1152 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-0 --primary --mode 1920x1080 --pos 1080x840 --rotate normal
